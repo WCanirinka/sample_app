e3d18d5a1252b8d61c2ab38996de1cfd90f0b603
Wilfried Canirinka <canirinkawilfried@gmail.com>
2019-12-10 21:56:25 +0200

    Complete advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,8 +7,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get FILL_IN
-    assert_response FILL_IN
+    get static_pages_home_url
+    assert_response :success
   end
 
   test "should get home" do
